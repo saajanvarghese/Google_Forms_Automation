4b6b7d15600af02958873b3f9a637ed329fdfe64
saajanvarghese <johnsaj97@gmail.com>
2024-03-27 11:48:04 +0530

    added comments, updated Gradle version and README.md

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
-# Project Name - _Project Author (Change Me)_
+# Project Name: Google Forms Automation
 
-## Project Name and Description:
-A concise yet descriptive title and an overview of the project’s purpose.
+## Google Forms Automation
+# Description
+The Application Under Test is Google Forms Automation.
+
+The Aim of this project is to Enter Values for WebElements present in the Google form and Submit the Google Form.
+
+This Google Form consists of Textbox, radio-button, checkBox, DateElement, Drop-down textBox.
+
+Written code in such a way that values are passed for each WebElement and  Submit the Google Form.
+
+For some WebElements, values are passed with some conitions
+1. For "Why are you Practicing Automation?" Field, we have to pass an input along with a variable value,
+2. For Date Field - Provided the current date minus 7 days in the date field, it should be dynamically calculated and not hardcoded.
+3. For "What is the Time Now?" -Provide the current time (Keeping in mind AM/PM) in the next field.
 
 ## Installation Instructions:
 Clear steps to set up and run the project locally.
@@ -10,15 +22,29 @@ Clear steps to set up and run the project locally.
 # java version 17
 java --version
 ```
+```
+# git version 2.43.0
+git --version
+```
+```
+# gradle 8.6
+gradle --version
+```
+
+```
+# vscode 1.87.2
+git --version
+```
 
 ## Usage and Examples:
 Provide sample code or usage scenarios to showcase how the project works.
 > Example:
 ```
-# to run the project
-./gradlew run
+# to build the project
+gradlew build
 ```
 
-## Important Links:
-Details about useful external links
- 
+```
+# to run the project
+gradlew run
+```
--- src/main/java/demo/TestCases.java
+++ src/main/java/demo/TestCases.java
@@ -33,83 +33,115 @@ public class TestCases {
     public  void testCase01() throws InterruptedException{
         System.out.println("Start Test case: testCase01");
 
+        // navigate to Google Forms link
         SeleniumWrapper.navigate(driver, "https://docs.google.com/forms/d/e/1FAIpQLSep9LTMntH5YqIXa5nkiPKSs283kdwitBBhXWyZdAS-e4CxBQ/viewform");
 
+        //Locate nameTextBox
         WebElement nameTextElement = driver.findElement(By.xpath("//div[@jscontroller='oCiKKc']//div[@class='Xb9hP']//input"));
 
+        //Send values to nameTextBox using Selenium Wrapper method
         SeleniumWrapper.clickEnterText(nameTextElement, "Saajan Varghese");
 
+        // Locate Practicing Automation TextBox
         WebElement practiceAutomation = driver.findElement(By.xpath("//div[@jscontroller='sWGJ4b']//textarea"));
 
         long epoch = System.currentTimeMillis()/1000;
 
+        //Send values to Practicing Automation using Selenium Wrapper method
         SeleniumWrapper.clickEnterText(practiceAutomation, "I want to be the best QA Engineer! " + epoch);
 
+        //Locate Experience radio-button
         WebElement experienceOptions = driver.findElement(By.xpath("//div[@class='nWQGrd zwllIb']//div[@id='i13' and @data-value = '0 - 2']"));
-
+        
+        //Click on radio-button using Selenium Wrapper method
         SeleniumWrapper.clickAction(experienceOptions, driver);
 
+        //Locate Java checkBox
         WebElement javaCheckBox = driver.findElement(By.xpath("//div[@class='eBFwI']//div[@id='i30' and @data-answer-value= 'Java']"));
 
+        //Locate Selenium checkBox
         WebElement SeleniumCheckBox = driver.findElement(By.xpath("//div[@class='eBFwI']//div[@id='i33' and @data-answer-value= 'Selenium']"));
 
+        //Locate TestNG checkBox
         WebElement TestNGCheckBox = driver.findElement(By.xpath("//div[@class='eBFwI']//div[@id='i39' and @data-answer-value= 'TestNG']"));
 
+        //Click on Java, Selenium, TestNG check-box
         SeleniumWrapper.checkBoxClick(javaCheckBox, SeleniumCheckBox, TestNGCheckBox, driver);
 
+        //Locate Select Tag
         WebElement selectTag = driver.findElement(By.xpath("//div[@jscontroller='liFoG']//div[@role='listbox']"));
 
+        //Click on SelectTag
         selectTag.click();
 
         Thread.sleep(3000);
 
+        // Locate Mr from Drop-Down TextBox
         WebElement SelectMr = driver.findElement(By.xpath("(//div[@jsname='wQNmvb']//span[text()='Mr'])[2]"));
 
+        // Select Mr from Drop-Down TextBox
         SeleniumWrapper.clickAction(SelectMr, driver);
 
         Thread.sleep(1000);
 
+        //Get the current Date and minus 7 Days  
         LocalDateTime myDateObj = LocalDateTime.now().minusDays(7);
+        //Set pattern for the Date
         DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 
+        //Converting Date to the pattern format
         String formatedDate = myDateObj.format(myFormatObj);
         System.out.println(formatedDate);
 
+        //Locate Date field
         WebElement dateField = driver.findElement(By.cssSelector("input[type='date']"));
-
+        //Clear Date field
         dateField.clear();
 
         Thread.sleep(3000);
 
+        //Send formated date as value
         dateField.sendKeys(formatedDate);
+        //Press Enter Key 
         dateField.sendKeys(Keys.ENTER);
 
+        // Get Current Time
        LocalTime myObj = LocalTime.now();
 
        System.out.println(myObj);
 
+       //Set pattern for Time
        DateTimeFormatter myFormatHourObj = DateTimeFormatter.ofPattern("HH");
-
+       //Set pattern for Time
        DateTimeFormatter myFormatMinuteObj = DateTimeFormatter.ofPattern("mm");
 
+       //Convert Time to the pattern format
        String formatedHour = myDateObj.format(myFormatHourObj);
 
+       //Convert Time to the pattern format
        String formatedMinute = myDateObj.format(myFormatMinuteObj);
 
+       //Locate Hours TextBox
        WebElement hours = driver.findElement(By.xpath("(//input[@type='text'])[2]"));
 
+       //Locate Minutes TextBox
        WebElement minutes = driver.findElement(By.xpath("(//input[@type='text'])[3]"));
 
+       // Enter Time
        SeleniumWrapper.EnterTimeField(hours, minutes, formatedHour, formatedMinute);
 
+       // Locate submit button
        WebElement submitbtn = driver.findElement(By.xpath("//span[text()='Submit']"));
 
+       // Click on Submit button
        submitbtn.click();
 
        Thread.sleep(3000);
 
+       // Find Element
        WebElement resultText = driver.findElement(By.xpath("//div[text()='Thanks for your response, Automation Wizard!']"));
 
+       //Get the Text from Element
        System.out.println(resultText.getText());
        System.out.println("end Test case: testCase01");
 
